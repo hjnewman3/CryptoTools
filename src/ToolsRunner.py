0c70cace6dbe1a88598707a7cb1ad0e9aa87eda2
Chip Newman <chip594@gmail.com>
2018-11-09 11:46:14 -0600

    added Runner file for testing

---
+++ src/ToolsRunner.py
@@ -0,0 +1,9 @@
+from CryptoTools import CryptoTools
+ct = CryptoTools()
+
+# runs a test on the RSA function
+print(ct.rsa(5, 11, 7, 5))
+
+# runs a test on the CRT function
+# verify that x = 721
+print(ct.crt(31, 37, 17, 2))
